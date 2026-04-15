259eaf2cec000812a76ef4597bf870e0d9b689a9
bambusgs <jjandus@wpi.edu>
2026-04-15 14:58:44 +0200

    added empty cuda files

---
+++ cuda/run_cuda.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#BSUB -J cuda
+#BSUB -q c02613
+#BSUB -W 30 
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=1024MB]"
+#BSUB -R "select[model == XeonE5_2660v3]"
+#BSUB -n 4
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -o cuda_%J.out
+#BSUB -e cuda_%J.err
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613_2026
+
+lscpu
+
+time python3 -u cuda.py 10
