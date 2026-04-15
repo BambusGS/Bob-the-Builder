fe77181862c32c5c04d9fa01aa318a224c3c877f
Johannes <johannes.hardenberg@hotmail.com>
2026-04-15 11:54:47 +0200

    working numba

--- numba/numba.sh
+++ numba/numba.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#BSUB -J run
+#BSUB -J numba
 #BSUB -q c02613
 #BSUB -W 30 
 #BSUB -R "span[hosts=1]"
@@ -8,11 +8,12 @@
 #BSUB -R "select[model == XeonE5_2660v3]"
 #BSUB -n 4
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -o run_%J.out
-#BSUB -e run_%J.err
-
+#BSUB -o numba_%J.out
+#BSUB -e numba_%J.err
 
 source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate 02613_2026
+echo "Hello from numba"
 
-python3 -u numba_analysis.py
+time python3 -u numba_analysis.py 10
+
