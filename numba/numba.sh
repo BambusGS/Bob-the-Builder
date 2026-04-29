604f9a26620434c53a297591c6e454b58de38901
Johannes <johannes.hardenberg@hotmail.com>
2026-04-29 11:33:15 +0200

    numba and parralell

--- numba/numba.sh
+++ numba/numba.sh
@@ -1,19 +1,27 @@
-#!/usr/bin/env bash
+g!/usr/bin/env bash
 
 #BSUB -J numba
 #BSUB -q c02613
-#BSUB -W 30 
+#BSUB -W  5
 #BSUB -R "span[hosts=1]"
-#BSUB -R "rusage[mem=1024MB]"
-#BSUB -R "select[model == XeonE5_2660v3]"
-#BSUB -n 4
+#BSUB -R "rusage[mem=16384MB]"
+#BSUB -n 8
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -o numba_%J.out
-#BSUB -e numba_%J.err
+#BSUB -o numba_all_%J.out
+#BSUB -e numba_all_%J.err
 
 source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate 02613_2026
 echo "Hello from numba"
 
-time python3 -u numba_analysis.py 10
+time python3 -u numba.py 1
+time python3 -u numba.py 1
+time python3 -u numba.py 1
+time python3 -u numba.py 1
+time python3 -u numba.py 1
+time python3 -u numba.py 1
+time python3 -u numba.py 1
+time python3 -u numba.py 1
+time python3 -u numba.py 1
+time python3 -u numba.py 1
 
